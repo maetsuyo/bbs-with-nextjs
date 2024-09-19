c41ca0da930574576363d96cbde5a9d80366b0bf
maetsuyo <maenoorehatuyokatta8009@gmail.com>
2024-09-19 16:00:35 +0900

    42:50

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import BBSCardList from "./components/BBSCardList";
 
-export default function Home() {
+export default async function Home() {
+  const res = await fetch("http://localhost:3000/api/post", {
+    cache: "no-cache",
+  });
   return (
     <main>
       <BBSCardList />
